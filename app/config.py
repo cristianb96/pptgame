79bdb61a9dfb2008fb14aa4ae0aa660ff4cece51
cristianb96 <55033854+cristianb96@users.noreply.github.com>
2024-09-22 20:02:29 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,16 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 
+db = SQLAlchemy()
 
 class Database:
     
     def __init__(self, app) -> None:
         load_dotenv()
         print("RC1 ", os.getenv("DATABASE_URL"))
-        app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://mateo:Sanmiguel99+@pptgame.postgres.database.azure.com:5432/test"#"postgresql://postgres:1234@localhost:5432/TEST_1"
+        app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://mateo:Sanmiguel99+@pptgame.postgres.database.azure.com:5432/test"
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         
         print("RCV ",app.config['SQLALCHEMY_DATABASE_URI'])
         
-        db = SQLAlchemy(app)
         db.init_app(app)
