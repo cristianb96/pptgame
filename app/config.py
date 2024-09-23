8860147c9064d0316f47577cf908643296a136e8
cristianb96 <55033854+cristianb96@users.noreply.github.com>
2024-09-22 19:00:40 -0500

    v4

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 class Database:
     
     def __init__(self, app) -> None:
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")#"postgresql://postgres:1234@localhost:5432/TEST_1"
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mateo:Sanmiguel99+@pptgame.postgres.database.azure.com:5432/test'#"postgresql://postgres:1234@localhost:5432/TEST_1"
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         
         db.init_app(app)
