bb98a969943225f639044fd0d250846f71751f91
cristianb96 <55033854+cristianb96@users.noreply.github.com>
2024-09-22 20:58:43 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 class Database:
     
     def __init__(self, app) -> None:
-        load_dotenv()
+        load_dotenv(find_dotenv())
         app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("CONNT_STRING")#"postgresql://mateo:Sanmiguel99+@pptgame.postgres.database.azure.com:5432/test"
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         db.init_app(app)
